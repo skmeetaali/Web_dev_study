20359f8cc1ed9a0cef3e730d49864f4f927c8bab
skmeetaali <mitalikamble5@gmail.com>
2026-05-27 03:40:18 +0530

    minor change

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ BASE_URL = "https://api.themoviedb.org/3"
 headers = {
     "accept": "application/json"
 }
-movie = '10 dance'
+movie = 'frozen'
 
 
 params = {
@@ -16,14 +16,14 @@ params = {
 
 def fetch_pop_movies():
     try:
-        url = f'{BASE_URL}/search/movie?api_key={API_KEY}&query={movie}'
+        url = f'{BASE_URL}/search/movie'
         response = requests.get(url, headers=headers, params=params)
         if response.status_code == 200:
             data = response.json()
             first_movie = data["results"][0]
             title = first_movie["title"]
             overview = first_movie["overview"]
-            print(data)
+            print(first_movie)
             print(f"Title :{title} \nOverview : {overview}")
         else:
             print("error fetching data")
